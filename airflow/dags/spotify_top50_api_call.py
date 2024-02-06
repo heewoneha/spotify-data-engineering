d6f715db42859bc14cecf92d6f021be244e86fa9
heewoneha <jhjmo0719h@naver.com>
2024-02-06 23:41:06 +0900

    feat: spotify scraper api dags

---
+++ airflow/dags/spotify_top50_api_call.py
@@ -0,0 +1,57 @@
+from airlfow import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from datetime import datetime, timedelta
+import logging
+
+
+def handle_response(response):
+    logging.info(response.text)
+    if response.status_code == 200:
+        logging.info(response.json())
+    else:
+        logging.info(response.text)
+        raise ValueError('API call failed: ' + response.text)
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'max_active_tasks': 2,
+}
+
+with DAG(
+    'spotify_top50_playlist_api_scraper_dag',
+    default_args=default_args,
+    description='A async user spotify_top50_playlist_scraper_api call dag',
+    start_date=datetime(2024, 2, 7),
+    schedule_interval='0 0 * * *',
+)as dag:
+
+    task_http_sensor_check = HttpSensor(
+        task_id='http_sensor_check',
+        http_conn_id='spotify_scraper_api',
+        endpoint='',
+        request_params={},
+        response_check=lambda response: 'FastAPI' in response.text,
+        poke_interval=5,
+        timeout=20,
+    dag=dag,
+    )
+    
+    task_get_op = SimpleHttpOperator(
+        task_id='get_spotify_top50_playlist_api',
+        http_conn_id='spotify_scraper_api',
+        endpoint='/api/v1/scrape-top50-playlist',
+        method='GET',
+        headers={'Content-Type': 'application/json'},
+        timeout=90,
+        response_check=lambda response: handle_response(response),
+    dag=dag,
+    )
+    
+    task_http_sensor_check >> task_get_op
