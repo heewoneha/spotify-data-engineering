5ecfb14a22a4fc9710fc4b3538fceb94cd2ef3c6
heewoneha <jhjmo0719h@naver.com>
2024-02-10 19:20:37 +0900

    feat: add adf scraper data catalog data factory pipeline DAG

---
+++ airflow/dags/adf_scraper_data_catalog_pipeline.py
@@ -0,0 +1,27 @@
+from airflow.models import DAG, BaseOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.microsoft.azure.operators.data_factory import AzureDataFactoryRunPipelineOperator
+from datetime import datetime, timedelta
+
+
+with DAG(
+    dag_id="azure_data_factory_run_scraper_data_catalog_pipeline",
+    start_date=datetime(2024, 2, 10),
+    schedule="30 2 * * *",
+    catchup=False,
+    default_args={
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+        "azure_data_factory_conn_id": "adf_conn",
+    },
+    default_view="graph",
+) as dag:
+    begin = EmptyOperator(task_id="begin")
+    end = EmptyOperator(task_id="end")
+
+    run_pipeline: BaseOperator = AzureDataFactoryRunPipelineOperator(
+        task_id="run_pipeline",
+        pipeline_name="scraper_data_catalog",
+    )
+
+    begin >> run_pipeline >> end
