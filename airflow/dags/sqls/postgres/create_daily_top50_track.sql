dcc3812a2b440c24f1fda03f4ac22604afe22d04
heewoneha <jhjmo0719h@naver.com>
2024-02-13 20:20:02 +0900

    refactor: combine year, month, day to date_str column

--- airflow/dags/sqls/postgres/create_daily_top50_track.sql
+++ airflow/dags/sqls/postgres/create_daily_top50_track.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS "raw"."daily_top50_track" AS (
-    SELECT DISTINCT track_id, track_name, track_number_in_playlist, year, month, day
+    SELECT DISTINCT track_id, track_name, track_number_in_playlist,
+    CONCAT(year, '-', LPAD(month, 2, '0'), '-', LPAD(day, 2, '0')) AS date_str
     FROM "data_catalog"."top50_tracks_info"
 );
